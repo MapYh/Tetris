41062729ab194fcecd21a63dd48e4e4a705e4173
MapYh <145336421+MapYh@users.noreply.github.com>
2024-09-06 13:50:14 +0200

    Added a way to place an offset to a square.

--- logic.js
+++ logic.js
@@ -1,9 +1,10 @@
 let playing_board = [];
-let playing_board_rows = 8;
-let playing_board_columns = 5;
+let playing_board_rows = 10;
+let playing_board_columns = 7;
 let x = 2;
 let y = 0;
 let init_flag = false;
+const square_size = 50;
 /********************Shapes    */
 
 let left_L = [
@@ -31,6 +32,23 @@ function init_shapes_and_place(playing_board) {
   console.log(playing_board);
 }
 
+function draw() {
+  const canvas = document.getElementById("playing_board");
+  //size of one tile on the board is 50 pixels.
+  canvas.width = 50 * playing_board_columns; // 350px.
+  canvas.height = 50 * playing_board_rows; // 500px.
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "rgb(200 0 0)";
+
+    ctx.fillStyle = "rgb(0 0 200 / 50%)";
+    //Blue square
+    for (let i = 0; i < playing_board_columns; i++) {
+      ctx.fillRect(50 * i, 50, square_size, square_size);
+    }
+  }
+}
+
 function play() {
   update_frame();
 }
@@ -38,12 +56,18 @@ function update_frame() {
   if (!init_flag) {
     init_Board();
   }
-  playing_board[y][x] = 0;
-  playing_board[y][x] = left_L;
+  if (!(x <= playing_board_columns || x >= 0)) {
+    playing_board[y][x] = 0;
+    playing_board[y][x] = left_L;
+  }
+  draw();
 }
 
 /********************Eventlisteners */
-//Change html node.
+
+window.addEventListener("load", draw);
+
+//Change html node to canvas.
 window.addEventListener(
   "keydown",
   function (event) {
