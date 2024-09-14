ed34f0efb237d3076bb6626ec67951c61aa7acfa
MapYh <145336421+MapYh@users.noreply.github.com>
2024-09-14 23:33:30 +0200

    Added some error checking for leeping track of game state (needs more work).

--- init.js
+++ init.js
@@ -6,6 +6,55 @@ let playing_board_columns = 10;
 let x = Math.floor((playing_board_columns - 1) / 2); //Create a function that choses the best starting coordinates.
 let y = 0;
 
+let shapes = {
+  vertical_line: [[1], [1], [1]],
+  horizontal_line: [[1, 1, 1]],
+  square: [
+    [1, 1],
+    [1, 1],
+  ],
+  right_L: [
+    [0, 0, 1],
+    [1, 1, 1],
+  ],
+  left_L: [
+    [1, 0, 0],
+    [1, 1, 1],
+  ],
+  right_L_standing: [
+    [1, 1],
+    [1, 0],
+    [1, 0],
+  ],
+  left_L_standing: [
+    [0, 1],
+    [0, 1],
+    [1, 1],
+  ],
+  zig_zag_right: [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  zig_zag_left: [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  zig_zag_standing_right: [
+    [0, 1],
+    [1, 1],
+    [1, 0],
+  ],
+  zig_zag_standing_left: [
+    [1, 0],
+    [1, 1],
+    [0, 1],
+  ],
+};
+
+var keys = Object.keys(shapes);
+let randnum = Math.floor(Math.random() * keys.length);
+let shape_key = keys[randnum];
+
 export default function init_Board() {
   playing_board = new Array(playing_board_rows);
   landed = new Array(playing_board_rows);
@@ -18,7 +67,7 @@ export default function init_Board() {
 
   //Init shapes on board.
   init_shapes_and_place(playing_board);
-  init_flag = true;
+
   return [
     playing_board,
     init_flag,
@@ -31,6 +80,15 @@ export default function init_Board() {
 
 function init_shapes_and_place(playing_board) {
   //PLaces in the starting location.
-  playing_board[y][x] = 1;
+  for (let i = 0; i < shapes[shape_key].length; i++) {
+    for (let j = 0; j < shapes[shape_key][i].length; j++) {
+      if (shapes[shape_key][i][j] != 0) {
+        playing_board[y + i][x + j] = 1;
+      }
+    }
+  }
+  init_flag = true;
   console.log(playing_board);
 }
+
+export { shape_key, randnum, keys, shapes, init_Board };
--- logic.js
+++ logic.js
@@ -1,56 +1,7 @@
-import init_board from "./init.js";
+import { init_Board, shape_key, randnum, keys, shapes } from "./init.js";
 
 /********************Shapes    */
 const square_size = 50;
-
-let shapes = {
-  vertical_line: [[1], [1], [1]],
-  horizontal_line: [[1, 1, 1]],
-  square: [
-    [1, 1],
-    [1, 1],
-  ],
-  right_L: [
-    [0, 0, 1],
-    [1, 1, 1],
-  ],
-  left_L: [
-    [1, 0, 0],
-    [1, 1, 1],
-  ],
-  right_L_standing: [
-    [1, 1],
-    [1, 0],
-    [1, 0],
-  ],
-  left_L_standing: [
-    [0, 1],
-    [0, 1],
-    [1, 1],
-  ],
-  zig_zag_right: [
-    [0, 1, 1],
-    [1, 1, 0],
-  ],
-  zig_zag_left: [
-    [1, 1, 0],
-    [0, 1, 1],
-  ],
-  zig_zag_standing_right: [
-    [0, 1],
-    [1, 1],
-    [1, 0],
-  ],
-  zig_zag_standing_left: [
-    [1, 0],
-    [1, 1],
-    [0, 1],
-  ],
-};
-
-var keys = Object.keys(shapes);
-let randnum = Math.floor(Math.random() * keys.length);
-let shape_key = keys[randnum];
 
 /*************Init playing board */
 let [
@@ -60,23 +11,20 @@ let [
   playing_board_columns,
   x,
   y,
-  landed,
-] = init_board();
+] = init_Board();
 
 function update_frame() {
   if (!init_flag) {
-    init_board();
+    init_Board();
   }
   draw();
 }
+
 /********************functions */
 
 function draw_Shape(canvas, ctx) {
   for (let i = 0; i < shapes[shape_key].length; i++) {
     for (let j = 0; j < shapes[shape_key][i].length; j++) {
-      /* if (landed[i][j] != 0) {
-        //draw landed block.
-      } */
       if (shapes[shape_key][i][j] != 0) {
         ctx.fillRect(
           50 * j + x * 50,
@@ -99,32 +47,31 @@ function draw() {
     ctx.fillStyle = "rgb(0 0 200 / 50%)";
     //When the x is within the playing field draw the shape on the canvas.
     if (x <= playing_board_columns && x >= 0) {
-      /* ctx.fillRect(50 * x, 50 * y, square_size, square_size); */
       draw_Shape(canvas, ctx, x, y, randnum);
     }
-
-    console.log("x", x);
-    console.log("y", y);
   }
-}
-
-//In x-cords.
-function tracking_game_state() {
-  //Places a one in the board array to keep track of the game state.
-  playing_board[y][x] = 1;
-
-  console.log(playing_board);
-  //Places a zero in the spot where the shape was in before moving.
-  playing_board[y][x - 1] = 0;
+  console.log("x", x);
+  console.log("y", y);
 }
 
 function play() {
   update_frame();
 }
+//Y-coord
+function tracking_game_state(x, y) {
+  //Places ones in the board array in the current shape to keep track of the game state.
+  for (let i = 0; i < shapes[shape_key].length; i++) {
+    for (let j = 0; j < shapes[shape_key][i].length; j++) {
+      if (shapes[shape_key][i][j] != 0) {
+        playing_board[y + i][x + j] = 1;
+
+        playing_board[y - 1][x + j] = 0;
+      }
+    }
+  }
+}
 
 /********************Eventlisteners */
-
-window.addEventListener("load", draw);
 
 //Change html node to canvas.
 window.addEventListener(
@@ -137,39 +84,24 @@ window.addEventListener(
       case "s":
       case "ArrowDown":
         if (y < playing_board_rows - shapes[shape_key].length && y >= 0) {
-          console.log("test", shapes[shape_key].length);
           y += 1;
           if (y == playing_board_rows) {
             y = playing_board_rows - shapes[shape_key].length;
-          }
-          //Places a one in the board array to keep track of the game state.
-          for (let i = 0; i < shapes[shape_key].length; i++) {
-            for (let j = 0; j < shapes[shape_key][i].length; j++) {
-              landed[i][j] = 1;
-              ctx.fillRect(
-                50 * j + x * 50,
-                50 * i + y * 50,
-                square_size,
-                square_size
-              );
-            }
+          } else {
+            tracking_game_state(x, y);
           }
 
           console.log(playing_board);
-          //Places a zero in the spot where the shape was in before moving.
-          playing_board[y - 1][x] = 0;
-          //Places a one in the board array to keep track of the game state.
         }
-        if (y == 0) {
+        /* if (y == 0) {
           y = 0;
-        }
+        } */
 
         break;
       case "a":
       case "ArrowLeft":
         if (x < playing_board_columns && x > 0) {
           x -= 1;
-          tracking_game_state(x, y);
         }
         if (x == 0) {
           x = 0;
@@ -181,7 +113,6 @@ window.addEventListener(
         //Less than or equal to zero on x so that the square dosen't get stuck on left side.
         if (x < playing_board_columns - shapes[shape_key][1].length && x >= 0) {
           x += 1;
-          tracking_game_state(x, y);
         }
         if (x == playing_board_columns) {
           x = playing_board_columns - 1;
@@ -190,7 +121,7 @@ window.addEventListener(
       default:
         return; // Quit when this doesn't handle the key event.
     }
-    update_frame();
+    draw();
     // Cancel the default action to avoid it being handled twice
     event.preventDefault();
   },
